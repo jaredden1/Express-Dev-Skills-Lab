6a6e34b177d05c640bf14dd8da223ea9256ab3b6
jaredden <james.ifit@gmail.com>
2023-08-01 18:16:40 -0400

    added models, controller, skills.js

--- server.js
+++ server.js
@@ -1,26 +1,30 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var cookieParser = require('cookie-parser'); //middleware designed to process cookies
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var skillsRouter = require('./routes/skills');
 
 var app = express();
 
 // view engine setup
+//app.set is used to configure apps
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//app.use is always used for middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// The first arg starts with path
+//The paths within the route modules are combined to the starts with paths
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/skills', skillsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
